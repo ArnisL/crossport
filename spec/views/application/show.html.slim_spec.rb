1f9fe1c310fa46bfe3d1e0b5468aaacce622fa4d
Arnis Lapsa <arnis.lapsa@gmail.com>
2016-10-31 20:57:46 +0200

    Add 'Open ticket' link to application#show

---
+++ spec/views/application/show.html.slim_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'application/show', type: :view do
+  context 'when authenticated' do
+    before do
+      sign_in User.create email: 'arnis.lapsa@gmail.com',
+        password: '123123', password_confirmation: '123123'
+    end
+
+    it "has 'Open ticket' link" do
+      render
+
+      expect(rendered).to have_css 'a', text: 'Open ticket'
+    end
+  end
+
+  context 'when anonymous' do
+    it "has no 'Open ticket' link" do
+      render
+
+      expect(rendered).not_to have_css 'a', text: 'Open ticket'
+    end
+  end
+end
