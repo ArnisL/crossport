69c77ef1cc133800629598f03cd6e6598902febc
Arnis Lapsa <arnis.lapsa@gmail.com>
2016-11-01 15:04:11 +0200

    Change ticket description to text_area

--- spec/views/tickets/new.html.slim_spec.rb
+++ spec/views/tickets/new.html.slim_spec.rb
@@ -1,25 +1,30 @@
 require 'rails_helper'
 
 describe 'tickets/new', type: :view do
-  it 'has new ticket form' do
-    @ticket = Ticket.new
+  before { @ticket = Ticket.new }
 
+  it 'has new ticket form' do
     render
 
     expect(rendered).to have_css 'form#new_ticket'
     expect(rendered).to have_css 'input#ticket_title'
-    expect(rendered).to have_css 'input#ticket_description'
+    expect(rendered).to have_css 'textarea#ticket_description'
     expect(rendered).to have_css \
       "form#new_ticket input[type='submit'][value='Open ticket']"
   end
 
   it 'shows ticket errors' do
-    @ticket = Ticket.new
     @ticket.valid?
 
     render
 
     expect(rendered).to have_css '.has-error input#ticket_title'
-    expect(rendered).to have_css '.has-error input#ticket_description'
+    expect(rendered).to have_css '.has-error textarea#ticket_description'
+  end
+
+  it 'has description text area with 10 rows' do
+    render
+
+    expect(rendered).to have_css "textarea#ticket_description[rows='10']"
   end
 end
