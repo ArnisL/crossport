375ecde6537c4db816a20e0483f4bc83a88a939f
Arnis Lapsa <arnis.lapsa@gmail.com>
2016-11-03 02:19:17 +0200

    Show email of authenticated user

--- spec/views/application/_header.html.slim_spec.rb
+++ spec/views/application/_header.html.slim_spec.rb
@@ -8,4 +8,25 @@ describe 'application/_header', type: :view do
       ".navbar-header .navbar-brand[href='#{root_path}']",
       text: 'CrossPort'
   end
+
+  it 'has sign out link if authenticated' do
+    render
+    expect(rendered).not_to have_css 'navbar-right a', text: 'Sign out'
+
+    sign_in create :user
+
+    render
+    expect(rendered).to have_css '.navbar-right a', text: 'Sign out'
+  end
+
+  it 'has user email if authenticated' do
+    render
+    expect(rendered).not_to have_css '.navbar-right .email'
+
+    user = create :user
+    sign_in user
+
+    render
+    expect(rendered).to have_css '.navbar-right .email.text-muted', text: user.email
+  end
 end
